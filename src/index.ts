042131c8689800465fa607f383293537a90bf5d1
Luke Zigler <luke.zigler98@gmail.com>
2025-04-28 13:58:45 +0100

    Updated index to implement createServer

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+import config from "config"
+import {createServer} from "./server/createServer";
+
 export function main(){
-    console.log("It's go time");
+    const port = config.get<string>("port");
+    const server = createServer();
+    server.listen(port, (error) => {
+        if(error){
+            console.error(error);
+            process.exit(1);
+        }
+        console.log("Server listening on port 3000");
+    });
 }
 
 main();
