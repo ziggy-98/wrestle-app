216a3814156c5321a10e428f170d172221df9b98
Luke <luke.zigler98@gmail.com>
2025-05-20 10:29:57 +0100

    Added functions for processing promotions

--- src/data/helpers/promotion.ts
+++ src/data/helpers/promotion.ts
@@ -1,17 +1,67 @@
-import {Browser} from "puppeteer";
+import {Browser, ElementHandle} from "puppeteer";
 import {Match, Promotion} from "../types";
+import config from "config";
 
-export async function getAllForMatches(browser: Browser, matches: Match[], promotions: Promotion[]): Promise<Promotion[]>{
-    let promotionsToReturn = promotions.slice(0);
+export async function getAllForMatchesAndMerge(browser: Browser, matches: Match[], promotions: Promotion[]): Promise<Promotion[]>{
+    const promotionsToReturn = promotions.slice(0);
     for(const match of matches){
         const matchPromotion = match.promotion.name;
         const existingPromotion = promotionsToReturn.find((promotion) => promotion.name === matchPromotion);
-        if(!existingPromotion){
-            const newPromotion = await getPromotionDetail(browser, match.promotion.url);
+        if(existingPromotion){
+            continue;
         }
+        console.info(`Retrieving data for promotion ${match.promotion.name}`);
+        const newPromotion = await getPromotionDetail(browser, match.promotion.url, match.promotion.name);
+        promotionsToReturn.push(newPromotion);
     }
+    return promotionsToReturn;
 }
 
-async function getPromotionDetail(browser: Browser, url: string): Promise<Promotion[]>{
-
+async function getPromotionDetail(browser: Browser, url: string, name: string): Promise<Promotion>{
+    let promotionToReturn: Partial<Promotion> = {
+        name
+    };
+    const BASE_URL = config.get<string>("data.baseUrl");
+    const page = await browser.newPage();
+    await page.goto(`${BASE_URL}${url}`);
+    const statusEl = page.$(".Caption::-p-text(General Data) + .InformationBoxTable .InformationBoxRow .InformationBoxTitle::-p-text(Status) + .InformationBoxContents");
+    const locationEl = page.$(".Caption::-p-text(General Data) + .InformationBoxTable .InformationBoxRow .InformationBoxTitle::-p-text(Location) + .InformationBoxContents");
+    const activeEl = page.$(".Caption::-p-text(General Data) + .InformationBoxTable .InformationBoxRow .InformationBoxTitle::-p-text(Active Time) + .InformationBoxContents");
+    const els = await Promise.allSettled([statusEl, locationEl, activeEl]);
+    const elsMap = {
+        status: els[0],
+        location: els[1],
+        active: els[2]
+    }
+    for(const [elName, promiseResult] of Object.entries(elsMap)){
+        const value = (promiseResult as PromiseFulfilledResult<ElementHandle | null>).value
+        if(!value){
+            console.warn(`Could not find any element for ${elName} on ${name}`);
+            continue;
+        }
+        if(elName === "status"){
+            promotionToReturn.isActive = await value.evaluate((evalEl) => evalEl.textContent ==="Active");
+            continue;
+        }
+        if(elName === "location"){
+            promotionToReturn.location = await value.evaluate((evalEl) => evalEl.textContent);
+            continue;
+        }
+        if(elName === "active"){
+            const {started, finished} = await value.evaluate((evalEl) => {
+                const [started, finished] = evalEl.textContent.split(" - ");
+                return {
+                    started,
+                    finished: finished === "today" ? undefined : finished
+                }
+            });
+            promotionToReturn = {
+                ...promotionToReturn,
+                started,
+                finished
+            }
+        }
+    }
+    await page.close();
+    return promotionToReturn as Promotion;
 }
